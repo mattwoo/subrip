2d60f8b2e6feed2d9351d28268ea555edfb5c24a
matt <vu.mateusz@gmail.com>
2016-11-08 14:42:38 +0100

    SubripPosition setters

--- src/Subrip/SubripPosition.php
+++ src/Subrip/SubripPosition.php
@@ -38,6 +38,38 @@ class SubripPosition implements SubripStyleInterface
     }
 
     /**
+     * @param mixed $x1
+     */
+    public function setX1($x1)
+    {
+        $this->x1 = $x1;
+    }
+
+    /**
+     * @param mixed $x2
+     */
+    public function setX2($x2)
+    {
+        $this->x2 = $x2;
+    }
+
+    /**
+     * @param mixed $y1
+     */
+    public function setY1($y1)
+    {
+        $this->y1 = $y1;
+    }
+
+    /**
+     * @param mixed $y2
+     */
+    public function setY2($y2)
+    {
+        $this->y2 = $y2;
+    }
+
+    /**
      * @return mixed
      */
     public function getX1()
